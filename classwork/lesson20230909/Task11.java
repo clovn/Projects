b88b2ea783afe9066b6e896004e1d0aea6a04f0b
clovn <haliullin101@mail.ru>
2023-09-15 18:09:47 +0300

    Task11 solved

--- classwork/lesson20230909/Task11.java
+++ classwork/lesson20230909/Task11.java
@@ -1,8 +1,34 @@
 public class Task11 {
 	public static void main(String[] args){
 		int n = Integer.valueOf(args[0]);
-		for(int i = 0; i < n; i++){
-			
+		int m = 1 + n*4;
+		int c = 1;
+		int empty = 2*n;
+		String symbol = " ";
+		for(int i = 1; i <= n*2; i++){
+			//System.out.println(i + " " + c + " " + empty);
+			for(int j = 1; j <= m;j++){
+				System.out.print(symbol);
+				if(j == empty){
+					symbol = "*";
+				} else if (j == c + empty) {
+					symbol = " ";
+				} else if (i > n && j == empty) {
+					symbol = "*";
+				} else if (i > n && j == empty + c){
+					symbol = " ";
+				} else if (i > n && j == m - empty - c){
+					symbol = "*";
+				} else if (i > n && j == m - empty){
+					symbol = " ";
+				}
+			}
+			empty -= 1;
+			c += 2;
+			System.out.println();
+			if(i == n){
+				c = 1;
+			}
 		}
 	}
 }
