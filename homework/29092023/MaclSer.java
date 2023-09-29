7a1eb4adb429ad9aa6f423d7e7b47c9b03e6ebdb
Clovn <Haliullin101@mail.ru>
2023-09-29 21:18:56 +0300

    recoded without library Math

--- homework/29092023/MaclSer.java
+++ homework/29092023/MaclSer.java
@@ -7,39 +7,20 @@ public class MaclSer{
         double res = 0.0;
         boolean right = false;
 
-        try{
-            Utils.setN(Integer.parseInt(args[2]));
-        } catch (Exception e){
-            switch (func){
-                case "sin":
-                    Utils.setN(20);
-                    break;
-                case "cos":
-                    Utils.setN(5);
-                    break;
-                case "e":
-                    Utils.setN(20);
-                    break;
-                case "ln1":
-                    Utils.setN(30);
-                    break;
-            }
-        }
-
         if ("sin".equals(func)) {
             double rads = Utils.degreesToRadians(x);
             res = Utils.sin(rads);
-            right = Utils.isRight(res, Math.sin(rads), 1e-7);
+            right = Utils.isRight(res, Math.sin(rads));
         } else if ("cos".equals(func)) {
             double rads = Utils.degreesToRadians(x);
             res = Utils.cos(rads);
-            right = Utils.isRight(res, Math.cos(rads), 1e-7);
+            right = Utils.isRight(res, Math.cos(rads));
         } else if ("e".equals(func)) {
             res = Utils.e(x);
-            right = Utils.isRight(res, Math.pow(Math.E, x), 1e-10);
+            right = Utils.isRight(res, Math.pow(Math.E, x));
         } else if ("ln1".equals(func)) {
             res = Utils.ln1(x);
-            right = Utils.isRight(res, Math.log1p(x), 1e-10);
+            right = Utils.isRight(res, Math.log1p(x));
         }
 
         System.out.println(res + " " + right);
@@ -47,67 +28,88 @@ public class MaclSer{
 }
 
 class Utils{
-    private static int N = 5;
-
-    public static void setN(int n){
-        N = n;
-    }
+    final static double ESP = 1e-10;
 
     public static double degreesToRadians(double degrees){
         return (degrees * Math.PI) / 180.0;
     }
 
-    public static boolean isRight(double result, double right, double delta){
-        return Math.abs(result - right) < delta;
+    public static boolean isRight(double result, double right){
+        return Math.abs(result - right) < 1e-7;
     }
 
     public static double e(double x){
-        double result = 1;
+        double result = 0;
         long fact = 1;
+        int ser = 1;
+        double deg = 1;
+        double term = deg / fact;
 
-        for(int i = 1; i < N; i++){
-            fact *= i;
-            result += Math.pow(x, i) / fact;
+        while (Math.abs(term)> ESP && ser < 20){
+            result += term;
+            fact *= ser;
+            ser += 1;
+            deg *= x;
+            term =  deg / fact;
         }
 
         return result;
     }
 
     public static double sin(double x) {
-        double sinValue = 0;
+        double result = 0;
         long fact = 1;
-        int f = 1;
-
-        for(int i = 1; i <= N;i++) {
-            sinValue += Math.pow(-1, i + 1) * Math.pow(x, 2*i - 1) / fact;
-            fact *= (long) (f + 1) *(f + 2);
-            f += 2;
+        int ser = 1;
+        int sign = 1;
+        double deg = x;
+        double term = sign * deg / fact;
+        while (Math.abs(term)>ESP && ser < 20){
+            result += term;
+            ser += 1;
+            fact *= (2*ser - 1)* (2*ser - 2);
+            sign = -1 * sign;
+            deg *= x * x;
+            term = sign * deg / fact;
         }
 
-        return sinValue;
+        return result;
     }
 
     public static double cos(double x) {
-        double cosValue = 0;
+        double result = 0;
         long fact = 1;
-        int f = 1;
+        int ser = 1;
+        int sign = 1;
+        double deg = 1;
+        double term = sign * deg / fact;
 
-        for(int i = 0; i < N;i++) {
-            cosValue += Math.pow(-1, i) * Math.pow(x, 2 * i) / fact;
-            for(int j = f; j <= f*2; j++){
-                fact *= j;
-            }
-            f *= 2;
+        while (Math.abs(term)>ESP && ser < 20){
+            result += term;
+            fact *= ser*(ser + 1);
+            ser += 1;
+            sign = -1 * sign;
+            deg *= x * x;
+            term = sign * deg / fact;
         }
 
-        return cosValue;
+        return result;
     }
 
     public static double ln1(double x){
         double result = 0;
+        long fact = 1;
+        int ser = 1;
+        int sign = 1;
+        double deg = x;
+        double term = sign * deg / ser;
 
-        for(int i = 1; i <= N; i++){
-            result += Math.pow(-1, i - 1)*Math.pow(x, i) / i;
+
+        while (Math.abs(term) > ESP && ser < 20){
+            result += term;
+            ser += 1;
+            sign = -1 * sign;
+            deg *= x;
+            term = sign * deg / ser;
         }
 
         return result;
